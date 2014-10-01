2f731814aa42e60bd21dee6b37472266281a3fd2
tuananhhcmus@gmail.com <tuananhhcmus@gmail.com@0bb60a18-ef82-33e0-1623-f830c38dd282>
2014-10-01 10:50:58 +0000

    ADD BASE_URL

--- trunk/App project/src/com/app/utils/Constant.java
+++ trunk/App project/src/com/app/utils/Constant.java
@@ -4,4 +4,6 @@ public final class Constant {
 
 	public static final String CHROME_PATH = "ReferenceLibraries/chromedriver.exe";
 	public static final String IE_PATH = "ReferenceLibraries/IEDriverServer.exe";
+	public static final String UIMAP_SINTEL = "src/UI Maps/SinTel.properties";
+	public static final String BASE_URL = "";
 }
