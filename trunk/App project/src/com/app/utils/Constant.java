e197e48c0ce8227254d186c175613b05ef3fc75d
tuananhhcmus@gmail.com <tuananhhcmus@gmail.com@0bb60a18-ef82-33e0-1623-f830c38dd282>
2014-10-01 10:26:09 +0000

    add Constant.java update Browser.java change chromePath variable

---
+++ trunk/App project/src/com/app/utils/Constant.java
@@ -0,0 +1,7 @@
+package com.app.utils;
+
+public final class Constant {
+
+	public static final String CHROME_PATH = "ReferenceLibraries/chromedriver.exe";
+	public static final String IE_PATH = "ReferenceLibraries/IEDriverServer.exe";
+}
