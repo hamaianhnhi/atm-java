ef9647f6b168d06abe0e0e9b1338856c34fa4006
tuananhhcmus@gmail.com <tuananhhcmus@gmail.com@0bb60a18-ef82-33e0-1623-f830c38dd282>
2014-10-02 04:31:28 +0000

    add SCREENSHOT_PATH

--- trunk/App project/src/com/app/utils/Constant.java
+++ trunk/App project/src/com/app/utils/Constant.java
@@ -7,5 +7,5 @@ public final class Constant {
 	public static final String UIMAP_SINTEL = "src/UI Maps/SinTel.properties";
 	public static final String BASE_URL = "";
 	public static final String DATASET = "src/Test Data/data1.xls";
-	public static final String SCREENSHOT_PATH = "src/Excution Reports/Failed Case ScreenShots";
+	public static final String SCREENSHOT_PATH = "src/Execution Reports/Failed Case ScreenShots/";
 }
