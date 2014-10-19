ded177b4dc4aff6c905338add83f4683064b9af9
tuananhhcmus@gmail.com <tuananhhcmus@gmail.com@0bb60a18-ef82-33e0-1623-f830c38dd282>
2014-10-19 13:29:50 +0000

    update

--- trunk/App project/src/com/app/systemadmin/ManageUsers.java
+++ trunk/App project/src/com/app/systemadmin/ManageUsers.java
@@ -4,10 +4,10 @@ package com.app.systemadmin;
 import java.io.IOException;
 import jxl.read.biff.BiffException;
 import static com.app.utils.Constant.DATASET;
-
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import static com.app.scenario.ManageUserScenario.*;
 import com.app.utils.ExcelReader;
 
 /**
@@ -25,16 +25,23 @@ public class ManageUsers extends BaseSuite {
 	    return data;
     }
 	
-	@Test(dataProvider = "invalid", description="dhauidyad", priority = 2, dependsOnMethods = {"test23"} )
-	public void validCase1(String username, String password, String expectResult) throws InterruptedException {
-		
-		  System.out.println("abc");
-
-	}
+//	@Test(dataProvider = "invalid", description="dhauidyad", priority = 2, dependsOnMethods = {"test23"} )
+//	public void validCase1(String username, String password, String expectResult) throws InterruptedException {
+//		
+//		  System.out.println("abc");
+//		  
+//
+//	}
 	
-	@Test(priority = 1)
-	public void test23() {
-		int a = 1/0;
+//	@Test(priority = 1)
+//	public void test23() {
+//		int a = 1/0;
+//	}
+	
+	@Test
+	public void deleteUser () {
+	
+		_deleteUser(getBrowser());
 	}
 	
 }
