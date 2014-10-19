41d0c20dd6f246cba6cafad9920bf7f91b82522f
tuananhhcmus@gmail.com <tuananhhcmus@gmail.com@0bb60a18-ef82-33e0-1623-f830c38dd282>
2014-10-19 13:29:50 +0000

    update

--- App project/src/com/app/scenario/ManageUserScenario.java
+++ App project/src/com/app/scenario/ManageUserScenario.java
@@ -1,10 +1,29 @@
 package com.app.scenario;
 
+
+import org.testng.Assert;
+
+import com.app.utils.Browser;
+import static com.app.utils.Constant.*;
+
+
 /**
  * This class contains all scenario for Manage User suite class
  * @author Chuot Con
  *
  */
+
 public class ManageUserScenario {
 
+	/**
+	 * delete user scenario
+	 */
+	public static void _deleteUser (Browser browser) {
+	   // navigaveTo();
+	   // findElement ();
+	   // Assert.assertEquals ();
+		System.out.println("in delete scenario");
+		browser.navigateTo(BASE_URL);
+		Assert.assertEquals(browser.getTitle(), "Google");
+	}
 }
--- App project/src/com/app/utils/Constant.java
+++ App project/src/com/app/utils/Constant.java
@@ -5,7 +5,7 @@ public final class Constant {
 	public static final String CHROME_PATH = "ReferenceLibraries/chromedriver.exe";
 	public static final String IE_PATH = "ReferenceLibraries/IEDriverServer.exe";
 	public static final String UIMAP_SINTEL = "src/UI Maps/SinTel.properties";
-	public static final String BASE_URL = "";
+	public static final String BASE_URL = "http://google.com";
 	public static final String DATASET = "src/Test Data/data1.xls";
 	public static final String SCREENSHOT_PATH = "src/Execution Reports/Failed Case ScreenShots/";
 }
